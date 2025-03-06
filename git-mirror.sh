4348a592c6052a586fbb6a4f24db8103e6546dc4
korvoj <korvoj@users.noreply.github.com>
2025-03-06 15:56:15 +0100

    Exteng the action to support mirroring a single branch

--- git-mirror.sh
+++ git-mirror.sh
@@ -6,6 +6,8 @@ SOURCE_REPO=$1
 DESTINATION_REPO=$2
 SOURCE_DIR=$(basename "$SOURCE_REPO")
 DRY_RUN=$3
+SINGLE_BRANCH=$4
+SINGLE_BRANCH_NAME=$5
 
 GIT_SSH_COMMAND="ssh -v"
 
@@ -13,16 +15,29 @@ echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 echo "DRY RUN=$DRY_RUN"
 
-git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
-git remote set-url --push origin "$DESTINATION_REPO"
-git fetch -p origin
-# Exclude refs created by GitHub for pull request.
-git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
-
-if [ "$DRY_RUN" = "true" ]
+if [ "SINGLE_BRANCH" = "true" ]
 then
-    echo "INFO: Dry Run, no data is pushed"
-    git push --mirror --dry-run
+    git clone --branch "$SINGLE_BRANCH_NAME" --single-branch "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
+    git fetch --prune origin
+    if [ "$DRY_RUN" = "true" ]
+    then
+        echo "INFO: Dry Run, no data is pushed"
+        git push --mirror --dry-run "$DESTINATION_REPO"
+    else
+        git push --mirror "$DESTINATION_REPO"
+    fi
 else
-    git push --mirror
+    git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
+    git remote set-url --push origin "$DESTINATION_REPO"
+    git fetch -p origin
+    # Exclude refs created by GitHub for pull request.
+    git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
+
+    if [ "$DRY_RUN" = "true" ]
+    then
+        echo "INFO: Dry Run, no data is pushed"
+        git push --mirror --dry-run
+    else
+        git push --mirror
+    fi
 fi
